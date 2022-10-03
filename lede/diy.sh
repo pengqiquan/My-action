e9292b9e642ced0954e6896bcc606ac278b0c1bc
mt <cloudorzi@gmail.com>
2022-10-03 12:24:02 +0800

    Update diy.sh

--- lede/diy.sh
+++ lede/diy.sh
@@ -8,13 +8,10 @@ rm -rf package/feeds/packages/smartdns
 mkdir -p package/custom && cd package/custom
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git
 git clone --depth=1 https://github.com/pymumu/smartdns.git
 git clone --depth=1 https://github.com/vernesong/OpenClash.git
 git clone --depth=1 https://github.com/fw876/helloworld.git
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git
+
 cd -
 ./scripts/feeds install -a
 
@@ -23,4 +20,4 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Modify some default settings
-curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lede/zzz-default-settings >package/lean/default-settings/files/zzz-default-settings
+# curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lede/zzz-default-settings >package/lean/default-settings/files/zzz-default-settings
